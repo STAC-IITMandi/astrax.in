1d7d2a6fdcc48ee2f096cc1aa0d5e9c097659444
harnaman-hk <harnaman.hk@gmail.com>
2021-04-01 19:53:54 +0530

    discard gatsby

--- gatsby/src/components/about2/about2.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function About2() {
-  return (
-    <section id="contribution" className="big-section bg-image-2 contribution">
-      <div className="container"></div>
-    </section>
-  );
-}
