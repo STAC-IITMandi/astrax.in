276af9a3423467227009cb3dfea68c2a6a6d4e35
Gajraj Singh Chouhan <54475046+gajrajgchouhan@users.noreply.github.com>
2021-04-07 20:44:49 +0530

    Template update (#35)
    * resized & converted images to webp
    
    * added responsive grid, added slider
    
    * * added accordion
    * removed unused css
    
    * * added https
    
    * fixed JS scroll
    
    * no async

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,31 @@
 /*
  * Change Navbar color while scrolling
-*/
+ */
 
-$(window).scroll(function(){
-	handleTopNavAnimation();
+$(function () {
+    if ($(window).scrollTop() > 10) {
+        $("#site-nav").addClass("navbar-solid");
+    } else {
+        $("#site-nav").removeClass("navbar-solid");
+    }
 });
 
-function handleTopNavAnimation() {
-	var top=$(window).scrollTop();
-
-	if(top>10){
-		$('#site-nav').addClass('navbar-solid'); 
-	}
-	else{
-		$('#site-nav').removeClass('navbar-solid'); 
-	}
-}
+$("#carousel").owlCarousel({
+    items: 1,
+    dots: true,
+    loop: true,
+    autoplay: true,
+    nav: true,
+    navText: [
+        '<div class="fa fa-chevron-left" aria-hidden="true"></div>',
+        '<div class="fa fa-chevron-right" aria-hidden="true"></div>',
+    ],
+    // responsive: {
+    //     768: {
+    //         items: 1,
+    //     },
+    //     1000: {
+    //         items: 2,
+    //     },
+    // },
+});
