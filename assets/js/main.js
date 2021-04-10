ec222459480d3e5262c6c06d97c378ae65104c6c
harnaman-hk <harnaman.hk@gmail.com>
2021-04-10 17:44:37 +0530

    layout for events section

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,52 @@ $(document).ready(function() {
     });
 });
 
+/*
+ * events Dropdown
+ */
+
+$(document).ready(function() {
+    var zindex = 10;
+
+    $("div.card").hover(function(e) {
+        e.preventDefault();
+
+        var isShowing = false;
+
+        if ($(this).hasClass("d-card-show")) {
+            isShowing = true
+        }
+
+        if ($("div.dashboard-cards").hasClass("showing")) {
+            // a card is already in view
+            $("div.card.d-card-show")
+                .removeClass("d-card-show");
+
+            if (isShowing) {
+                // this card was showing - reset the grid
+                $("div.dashboard-cards")
+                    .removeClass("showing");
+            } else {
+                // this card isn't showing - get in with it
+                $(this)
+                    .css({ zIndex: zindex })
+                    .addClass("d-card-show");
+
+            }
+            zindex++;
+        } else {
+            // no dashboard-cards in view
+            $("div.dashboard-cards")
+                .addClass("showing");
+            $(this)
+                .css({ zIndex: zindex })
+                .addClass("d-card-show");
+            zindex++;
+        }
+
+    });
+});
+
 $("#carousel").owlCarousel({
     items: 1,
     dots: true,
