b5c8596c0b77276dc45bc757047d7e8d3f8f7f8f
gd-codes <45459312+gd-codes@users.noreply.github.com>
2021-04-13 11:27:26 +0530

    Mobile view navbar improv
    - On small screens, navbar menu has an opaque grey bg when expanded while at the top of the page
    - Menu collapses when a link is clicked
    - leading slash in path on index.html line 59 removed as it is interpreted as local file address on *nix OSes, image didn't used to load

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,24 @@
  */
 
 $(document).ready(function() {
+    //Make site navbar opaque
     $(document).scroll(function() {
-        if ($(this).scrollTop() > 10) {
+        if ($(this).scrollTop() > 100) {
             $("#site-nav").addClass("navbar-solid");
         } else {
             $("#site-nav").removeClass("navbar-solid");
         }
+    });
+    //Grey background instead of transparent on small screens (collapsing menu)
+    $("#navbar-items").on('show.bs.collapse', function() {
+        $("#site-nav").addClass("navbar-solid-header");
+    });
+    $("#navbar-items").on('hide.bs.collapse', function() {
+        $("#site-nav").removeClass("navbar-solid-header");
+    });
+    //Close navbar when link is clicked
+    $("#navbar-items .nav-item").click(function(clickevt) {
+        $("#navbar-items").collapse('hide');
     });
 });
 
