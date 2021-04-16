93f86c4b393f8a29c1456058da5282f36f35e624
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2021-04-16 11:04:14 +0530

    added countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,30 @@ $(document).ready(function() {
     });
 });
 
+/* 
+ * Logo Shrink
+ */
+
+window.onscroll = function() {
+    growShrinkLogo()
+};
+  
+var Logo = $("#navbar-logo");
+var endOfDocumentTop = 100;
+var size = 0;
+
+function growShrinkLogo() {
+    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (size == 0 && scroll > endOfDocumentTop) {
+        Logo.addClass('smallLogo');
+        size = 1;
+    } else if(size == 1 && scroll <= endOfDocumentTop){
+        Logo.removeClass('smallLogo');
+        size = 0;
+    }
+}
+
 /*
  * Owl Carousel
  */
@@ -79,3 +103,28 @@ function scrollToTop() {
 }
 scrollToTopBtn.off().on("click", scrollToTop);
 
+// Countdown
+
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    let astrax = "May 14 2021 00:00:00 GMT+0530",
+        countDown = new Date(astrax).getTime(),
+        x = setInterval(function() {
+          let now = new Date().getTime(),
+              distance = countDown - now;
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+          //do something later when date is reached
+          if (distance < 0) {
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+}());
