52cfb47fd3106efd3d4dd8352b76a1b479c80418
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2021-05-07 15:01:34 +0530

    added future in countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         $("#site-nav").removeClass("navbar-solid-header");
     });
     //Close navbar when link is clicked
-    $("#navbar-items .nav-item").click(function(clickevt) {
+    $("#navbar-items .nav-item").click(function() {
         $("#navbar-items").collapse('hide');
     });
 });
@@ -71,44 +71,6 @@ $(document).ready(function() {
         }
 
     });
-
-    // $("div.card").click(function(e) {
-    //     e.preventDefault();
-
-    //     var isShowing = false;
-
-    //     if ($(this).hasClass("d-card-show")) {
-    //         isShowing = true
-    //     }
-
-    //     if ($("div.dashboard-cards").hasClass("showing")) {
-    //         // a card is already in view
-    //         $("div.card.d-card-show")
-    //             .removeClass("d-card-show");
-
-    //         if (isShowing) {
-    //             // this card was showing - reset the grid
-    //             $("div.dashboard-cards")
-    //                 .removeClass("showing");
-    //         } else {
-    //             // this card isn't showing - get in with it
-    //             $(this)
-    //                 .css({ zIndex: zindex })
-    //                 .addClass("d-card-show");
-
-    //         }
-    //         zindex++;
-    //     } else {
-    //         // no dashboard-cards in view
-    //         $("div.dashboard-cards")
-    //             .addClass("showing");
-    //         $(this)
-    //             .css({ zIndex: zindex })
-    //             .addClass("d-card-show");
-    //         zindex++;
-    //     }
-
-    // });
 });
 
 /*
@@ -125,15 +87,7 @@ $("#carousel").owlCarousel({
     navText: [
         '<div class="fa fa-chevron-left" aria-hidden="true"></div>',
         '<div class="fa fa-chevron-right" aria-hidden="true"></div>',
-    ],
-    // responsive: {
-    //     768: {
-    //         items: 1,
-    //     },
-    //     1000: {
-    //         items: 2,
-    //     },
-    // },
+    ]
 });
 
 /*
@@ -168,18 +122,32 @@ scrollToTopBtn.off().on("click", scrollToTop);
         hour = minute * 60,
         day = hour * 24;
 
-    let astrax = "May 14 2021 00:00:00 GMT+0530",
-        countDown = new Date(astrax).getTime(),
-        x = setInterval(function() {
+    let astraXstart = "May 14 2021 00:00:00 GMT+0530",
+        astraXend = "May 16 2021 23:59:59 GMT+0530",
+        countDownstart = new Date(astraXstart).getTime(),
+        countDownend = new Date(astraXend).getTime(),
+        x = setInterval(() =>
+        {
             let now = new Date().getTime(),
-                distance = countDown - now;
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+                distance = countDownstart - now;
+            document.getElementById("days").innerText = (distance > 0) ? (Math.floor(distance / (day))) : (0),
+            document.getElementById("hours").innerText = (distance > 0) ? (Math.floor((distance % (day)) / (hour))) : (0),
+            document.getElementById("minutes").innerText = (distance > 0) ? (Math.floor((distance % (hour)) / (minute))) : (0),
+            document.getElementById("seconds").innerText = (distance > 0) ? (Math.floor((distance % (minute)) / (second))) : (0);
 
             //do something later when date is reached
             if (distance < 0) {
+                const countdown = document.getElementById("countdown");
+                if (now >= countDownstart && now <= countDownend) {
+                    countdown.innerHTML = 'AstraX 21 has started!';
+                    countdown.removeAttribute("class");
+                    countdown.style.fontSize = '1.4rem';
+                }
+                else if (now > countDownend) {
+                    countdown.innerHTML = 'AstraX 21 has ended! See you next year!';
+                    countdown.removeAttribute("class");
+                    countdown.style.fontSize = '1.4rem';
+                }
                 clearInterval(x);
             }
             //seconds
